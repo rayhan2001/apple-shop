41ea56649bc24b33bac7d4229b220861447caec4
Md Rayhan Ali <78335557+rayhan2001@users.noreply.github.com>
2025-04-23 23:02:13 +0600

    route defile complete

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\PolicyController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('brands', BrandController::class)->only([
     'index', 'create', 'store', 'update', 'destroy'
 ]);
 Route::resource('categories', CategoryController::class);
+Route::resource('invoices', InvoiceController::class);
+Route::resource('policies', PolicyController::class);
+Route::resource('products', ProductController::class);
+Route::resource('profiles', ProfileController::class);
