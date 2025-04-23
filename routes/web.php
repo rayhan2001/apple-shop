c8fed68a94c378fad68774221d6c8ca319b01af0
Md Rayhan Ali <78335557+rayhan2001@users.noreply.github.com>
2025-04-23 23:01:56 +0600

    controller create and route define complete

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('brands', BrandController::class)->only([
+    'index', 'create', 'store', 'update', 'destroy'
+]);
+Route::resource('categories', CategoryController::class);
